20d9ab89bab0b088517cd7b5a19250dece1b3a97
immanuelvatta <immanuelvatta@gmail.com>
2023-09-26 20:01:07 -0500

    9/26/2023

--- TwoSum.js
+++ TwoSum.js
@@ -35,3 +35,15 @@ var twoSum = function (nums, target) {
   }
   return [];
 };
+
+nums1 = [2,7,11,15]
+target1 = 9
+console.log(twoSum(nums1, target1))
+
+nums2 = [3,2,4]
+target2 = 6
+console.log(twoSum(nums2, target2))
+
+nums3 = [3,3]
+target3 = 6
+console.log(twoSum(nums3, target3))
